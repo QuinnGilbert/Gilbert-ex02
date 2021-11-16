2d495a810bd38313ec5c282360f16e06b8ac6dd0
QuinnG <quinngilbert12and3@gmail.com>
2021-11-16 10:06:17 -0500

    First Commit

---
+++ src/main/java/Solution02.java
@@ -0,0 +1,16 @@
+/*
+ *  UCF COP3330 Fall 2021 Exercise 2
+ *  Copyright 2021 Quinn Gilbert
+ */
+
+import java.util.*;
+public class Solution02 {
+    public static void main(String[] args) {
+        //print prompt
+        System.out.print("What is the input string? ");
+        //input string
+        String s = new Scanner(System.in).nextLine();
+        //print output
+        System.out.println(s+" has "+s.length()+" characters");
+    }
+}
